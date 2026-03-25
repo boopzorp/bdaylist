a4d7550f9d111dfff705fd8ded37d73fc5ee842a
Manvendra Upadhyay <manvendraupadhyay.cs@gmail.com>
2026-03-25 17:53:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7000781957-a8a0a",
+  "appId": "1:598618500149:web:c0180bc3e8653ed33c557c",
+  "apiKey": "AIzaSyDdK-jHfT-c_kgaiLraykkd3O_uMSnUFe4",
+  "authDomain": "studio-7000781957-a8a0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "598618500149"
+};
